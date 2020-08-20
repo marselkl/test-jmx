0b14957dbadc7d5fc84fa5dee1ac046eadd7694d
marsel <tktelufa@gmail.com>
2020-08-20 14:27:20 +0500

    add app

--- app/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-java -Dcom.sun.management.jmxremote \
-     -Dcom.sun.management.jmxremote.port=1617 \
-     -Dcom.sun.management.jmxremote.authenticate=false \
-     -Dcom.sun.management.jmxremote.ssl=false \
-     SimpleAgent
